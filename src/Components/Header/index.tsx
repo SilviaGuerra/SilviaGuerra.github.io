5b8cfdbe96d8bfbc473a72cdb6e20fcbeb5a4da7
SilviaGuerra <silvia.guerrahdz@gmail.com>
2023-02-28 22:13:38 -0600

    Add images and ant

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Menu } from "antd";
 
 const Header = () => {
-  return <div>Heeeadeerr</div>;
+  return (
+    <>
+      <div className="logo" />
+      <Menu
+        // theme="dark"
+        // className="header-"
+        mode="horizontal"
+        defaultSelectedKeys={["2"]}
+        items={new Array(15).fill(null).map((_, index) => {
+          const key = index + 1;
+          return {
+            key,
+            label: `nav ${key}`,
+          };
+        })}
+      />
+    </>
+  );
 };
 
 export default Header;
